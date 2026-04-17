f42dfb855c5d916750627a81df4d7ed8231e8f9e
Md Mahmud Hossain Ferdous <mahmudhossainferdous@gmail.com>
2026-04-17 22:27:43 +0600

    Updated app name

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp.router(
-      title: 'Notes App',
+      title: 'My Notes',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
